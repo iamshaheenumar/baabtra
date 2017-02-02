517bf6f63bb47dac0aff90f847c6edad04f50b85
iamshaheenumar <iamshaheenumar@gmail.com>
2017-02-03 01:10:02 +0530

    full responsive

--- Facebook/js/fblogin.js
+++ Facebook/js/fblogin.js
@@ -1,1 +1,16 @@
+$(document).ready(function(){
+  addPlaceholder();
+  $(window).resize(function(){addPlaceholder();})
+  });
+  function addPlaceholder() {
+    if ($(window).width() < 991 ) {
+      $("#username").attr("placeholder","Email address or phone number");
+      $("#password").attr("placeholder","Password");
+    }
+    else {
+      $("#username").attr("placeholder","");
+      $("#password").attr("placeholder","");
 
+    }
+
+  }
